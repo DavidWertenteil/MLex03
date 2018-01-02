165285a2f52d3328b35861c5121f713c78090146
davidwer <davhadassah@gmail.com>
2018-01-02 21:49:06 +0200

    First

--- MLex03.py
+++ MLex03.py
@@ -11,7 +11,8 @@ from sklearn import linear_model, datasets, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import BernoulliRBM
 from sklearn.pipeline import Pipeline
-
+from sklearn.metrics import precision_score
+import time
 
 # #############################################################################
 # Setting up
@@ -63,51 +64,51 @@ rbm = BernoulliRBM(random_state=0, verbose=True)
 
 classifier = Pipeline(steps=[('rbm', rbm), ('logistic', logistic)])
 
-# #############################################################################
-# Training
+# ########################### 1 #############################################
+time_each_training = []
+precisions_RBM = []
+precisions_raw = []
 
-# Hyper-parameters. These were set by cross-validation,
-# using a GridSearchCV. Here we are not performing cross-validation to
-# save time.
-rbm.learning_rate = 0.06
-rbm.n_iter = 20
-# More components tend to give better prediction performance, but larger
-# fitting time
-rbm.n_components = 100
-logistic.C = 6000.0
 
-# Training RBM-Logistic Pipeline
-classifier.fit(X_train, Y_train)
+def training(n):
+    # Training
 
-# Training Logistic regression
-logistic_classifier = linear_model.LogisticRegression(C=100.0)
-logistic_classifier.fit(X_train, Y_train)
+    # Hyper-parameters. These were set by cross-validation,
+    # using a GridSearchCV. Here we are not performing cross-validation to
+    # save time.
+    rbm.learning_rate = 0.06
+    rbm.n_iter = 20
+    # More components tend to give better prediction performance, but larger
+    # fitting time
+    rbm.n_components = n ** 2
+    logistic.C = 6000.0
 
-# #############################################################################
-# Evaluation
+    # Training RBM-Logistic Pipeline
+    time.time()
+    classifier.fit(X_train, Y_train)
+    time_each_training.append((n**2, time.time()))
 
-# print()
-# print("Logistic regression using RBM features:\n%s\n" % (
-#     metrics.classification_report(
-#         Y_test,
-#         classifier.predict(X_test))))
-#
-# print("Logistic regression using raw pixel features:\n%s\n" % (
-#     metrics.classification_report(
-#         Y_test,
-#         logistic_classifier.predict(X_test))))
+    # Training Logistic regression
+    logistic_classifier = linear_model.LogisticRegression(C=100.0)
+    logistic_classifier.fit(X_train, Y_train)
 
-# #############################################################################
-# Plotting
+    precisions_RBM.append(precision_score(Y_test, classifier.predict(X_test), average='macro'))
+    precisions_raw.append(precision_score(Y_test, logistic_classifier.predict(X_test), average='macro'))
 
-# plt.figure(figsize=(4.2, 4))
-# for i, comp in enumerate(rbm.components_):
-#     plt.subplot(10, 10, i + 1)
-#     plt.imshow(comp.reshape((8, 8)), cmap=plt.cm.gray_r,
-#                interpolation='nearest')
-#     plt.xticks(())
-#     plt.yticks(())
-# plt.suptitle('100 components extracted by RBM', fontsize=16)
-# plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
-#
-# plt.show()
+    #  ----------------------------- Plotting -------------------------------
+    plt.figure(figsize=(4.2, 4))
+    for i, comp in enumerate(rbm.components_):
+        plt.subplot(n, n, i + 1)
+        plt.imshow(comp.reshape((8, 8)), cmap=plt.cm.gray_r,
+                   interpolation='nearest')
+        plt.xticks(())
+        plt.yticks(())
+    plt.suptitle(str(n**2) + ' components extracted by RBM', fontsize=16)
+    plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+
+
+
+for i in range(2, 21):
+    training(i)
+plt.show()
+
